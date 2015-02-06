5ea086af81e8fa1bdd46167614958378ab0c78c2
Ubuntu <ubuntu@ip-172-31-52-179.ec2.internal>
2015-02-06 12:40:17 +0000

    Add node app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var express = require('express');
- 
-var app = express();
- 
+var express = require('express')
+var app = express();  
+
+app.set('port', process.env.PORT || 8080)
+app.use(express.static(__dirname + '/public'))  
 app.get('/', function (req, res) {
   res.send('hello world');
-});
+})
  
-app.listen(process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+console.log('Express server started on port', app.get('port'))
+}) 
  
-module.exports = app
+module.exports = app;
